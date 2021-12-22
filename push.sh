0bb9818134ba10f7c2d87b04f76ff8e88a07f586
Quitino <fb_941219@163.com>
2021-12-22 14:05:45 +0800

    first commit

---
+++ push.sh
@@ -0,0 +1,7 @@
+###
+ # @Date: 2021-12-22 14:03:52
+ # @Author: bFeng
+### 
+git add .                      #添加到暂存区，可以将.换成具体文件的名称
+git commit -m "updata"        #本地做提交
+git push origin master         #把本地master分支的最新修改推送至GitHub
