589dd57c180dda22d3bad4397a7ead7191329926
Quitino <fb_941219@163.com>
2021-12-22 14:16:56 +0800

    updata

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@
 ### 
 git add .                      #添加到暂存区，可以将.换成具体文件的名称
 git commit -m "updata"        #本地做提交
-git push origin master         #把本地master分支的最新修改推送至GitHub
+git push origin main         #把本地master分支的最新修改推送至GitHub
